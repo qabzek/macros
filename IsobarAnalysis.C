28bae7af8275f5e96b8332725db7a5a3e2325473
qabzek <kazbekkoblan3@gmail.com>
2023-07-14 18:55:11 +0300

    gfdfsgh

--- IsobarAnalysis.C
+++ IsobarAnalysis.C
@@ -17,25 +17,35 @@
 #include "TCanvas.h"
 
 // PicoDst headers
-#include "../StPicoEvent/StPicoDstReader.h"
-#include "../StPicoEvent/StPicoDst.h"
-#include "../StPicoEvent/StPicoEvent.h"
-#include "../StPicoEvent/StPicoTrack.h"
-#include "../StPicoEvent/StPicoBTofHit.h"
-#include "../StPicoEvent/StPicoBTowHit.h"
-#include "../StPicoEvent/StPicoEmcTrigger.h"
-#include "../StPicoEvent/StPicoBTofPidTraits.h"
-#include "../StPicoEvent/StPicoTrackCovMatrix.h"
+#include "StPicoEvent/StPicoDstReader.h"
+#include "StPicoEvent/StPicoDst.h"
+#include "StPicoEvent/StPicoEvent.h"
+#include "StPicoEvent/StPicoTrack.h"
+#include "StPicoEvent/StPicoBTofHit.h"
+#include "StPicoEvent/StPicoBTowHit.h"
+#include "StPicoEvent/StPicoEmcTrigger.h"
+#include "StPicoEvent/StPicoBTofPidTraits.h"
+#include "StPicoEvent/StPicoTrackCovMatrix.h"
+#include "StRefMultCorr/StRefMultCorr.h"
+#include "StRefMultCorr/BadRun.h"
+#include "StRefMultCorr/CentralityMaker.h"
+#include "StRefMultCorr/Param.h"
 //_________________
 #if ROOT_VERSION_CODE >= ROOT_VERSION(6, 0, 0)
-R__LOAD_LIBRARY(../StPicoEvent/libStPicoDst);
+R__LOAD_LIBRARY(StPicoEvent/libStPicoDst);
 #else
-gSystem->Load("../StPicoEvent/libStPicoDst.so");
+gSystem->Load("StPicoEvent/libStPicoDst.so");
 #endif
 
-const Char_t *defaultOutFile = "19.6GeV_Test.root";
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6, 0, 0)
+R__LOAD_LIBRARY(StRefMultCorr/libStRefMultCorr);
+#else
+gSystem->Load("StPicoEvent/libStRefMultCorr.so");
+#endif
 
-int AnalysisModified(const Char_t *inFile = "/home/ubuntu/RootFiles/st_physics_*.picoDst.root", const Char_t *outFileName = defaultOutFile)
+const Char_t *defaultOutFile = "../1Myresults/isobar_Test.root";
+
+int IsobarAnalysis(const Char_t *inFile = "/home/ubuntu/isobar_files/alisher/st_physics_*.picoDst.root", const Char_t *outFileName = defaultOutFile)
 {
 
     StPicoDstReader *picoReader = new StPicoDstReader(inFile);
@@ -50,10 +60,8 @@ int AnalysisModified(const Char_t *inFile = "/home/ubuntu/RootFiles/st_physics_*
 
     TFile *outputFile = new TFile(defaultOutFile, "recreate");
 
-    float cent5=-999;
-
     // histogram building
-    
+
     build();
 
     Long64_t events2read = picoReader->chain()->GetEntries();
@@ -84,16 +92,173 @@ int AnalysisModified(const Char_t *inFile = "/home/ubuntu/RootFiles/st_physics_*
                       << std::endl;
             break;
         }
+        Long64_t events2read = picoReader->chain()->GetEntries();
+        for (Long64_t iEvent = 0; iEvent < events2read; iEvent++)
+        {
 
-        float RefM=event->refMult();
-        if      (RefM > 209)                cent5 = 0; //0-10
-        else if (RefM > 146 && RefM <= 209) cent5 = 1; //10-20
-        else if (RefM > 66  && RefM <= 146) cent5 = 2; //20-40
-        else if (RefM > 25  && RefM <= 66 ) cent5 = 3; //40-60
-        else if (RefM > 8   && RefM <= 25 ) cent5 = 4; //60-80
-        else cent5 =-1;
-        if  (cent5 < 0) continue;
+            Int_t multCounter = 0;
 
+            if (iEvent % 1000 == 0)
+                std::cout << "Working on event #[" << (iEvent + 1)
+                          << "/" << events2read << "]" << std::endl;
 
+            if (!(picoReader->readPicoEvent(iEvent)))
+            {
+                std::cout << "Something went wrong, Master! Nothing to analyze..."
+                          << std::endl;
+                break;
+            }
+
+            // Retrieve picoDst
+            StPicoDst *picoDst = picoReader->picoDst();
+
+            // Retrieve event information
+            StPicoEvent *event = picoDst->event();
+            if (!event)
+            {
+                std::cout << "Something went wrong, Master! Event is hiding from me..."
+                          << std::endl;
+                break;
+            }
+
+            // cut badruns PileUp and make centrality
+            hist1[0][0]->Fill(event->refMult());
+            // Vxy
+            hist2[0][11]->Fill(event->primaryVertex().X(), event->primaryVertex().Y());
+            // Vz
+            hist1[0][1]->Fill(event->primaryVertex().Z());
+
+            int cent9 = -999;
+            double CentWeight;
+            StRefMultCorr *refmultCorrUtil = CentralityMaker::instance()->getRefMultCorr();
+            refmultCorrUtil->init(event->runId());
+            Bool_t isBadRun = refmultCorrUtil->isBadRun(event->runId());
+            if (refmultCorrUtil->isPileUpEvent(event->refMult(), event->nBTOFMatch(), event->primaryVertex().Z()))
+                continue;
+            refmultCorrUtil->initEvent(event->refMult(), event->primaryVertex().Z(), event->ZDCx());
+            cent9 = refmultCorrUtil->getCentralityBin9();
+            CentWeight = refmultCorrUtil->getWeight();
+            if (isBadRun)
+                continue;
+            if (cent9 < 0)
+                continue;
+
+            // refmult
+
+            // same with cuts
+            if (event->primaryVertex().Z() < 25 && event->primaryVertex().Z() > -35 &&
+                TMath::Sqrt(pow(event->primaryVertex().X(), 2) + pow(event->primaryVertex().Y(), 2)) < 2)
+            {
+                // Vxy
+                hist2[1][11]->Fill(event->primaryVertex().X(), event->primaryVertex().Y());
+                // Vz
+                hist1[1][1]->Fill(event->primaryVertex().Z());
+            }
+
+            Int_t nTracks = picoDst->numberOfTracks();
+            for (Int_t iTrack = 0; iTrack < nTracks; iTrack++)
+            {
+                StPicoTrack *picoTrack = picoDst->track(iTrack);
+                Float_t flFit, flPoss;
+                flFit = static_cast<float>(picoTrack->nHitsFit());
+                flPoss = static_cast<float>(picoTrack->nHitsPoss());
+                if (!picoTrack)
+                    continue;
+
+                if (picoTrack->isPrimary())
+                {
+                    hist1[0][2]->Fill(picoTrack->pMom().Eta());
+                    // fill primary track hits
+                    hist1[0][3]->Fill(picoTrack->nHits());
+                    hist1[0][4]->Fill(picoTrack->nHitsFit());
+                    hist1[0][5]->Fill(picoTrack->nHitsMax());
+                    hist1[0][6]->Fill(flFit / flPoss);
+                    // fill DCA
+                    hist1[0][7]->Fill(picoTrack->gDCA(event->primaryVertex()).Z());
+                    hist1[0][8]->Fill(picoTrack->gDCA(event->primaryVertex()).Mag());
+                    hist1[0][9]->Fill(picoTrack->gDCA(event->primaryVertex()).X(), picoTrack->gDCA(event->primaryVertex()).Y());
+                    // fill dE/dx hits
+                    hist1[0][10]->Fill(picoTrack->nHitsDedx());
+                    // fill dEdx p/q
+                    hist2[0][12]->Fill(picoTrack->pPtot() / picoTrack->charge(), picoTrack->dEdx());
+                    // fill Phi vs Pt
+                    hist2[0][14]->Fill(picoTrack->pPt(), picoTrack->pMom().Phi());
+                    // fill Eta vs Pt
+                    hist2[0][15]->Fill(picoTrack->pPt(), picoTrack->pMom().Eta());
+
+                    // fill for TOF
+                    if (picoTrack->isTofTrack())
+                    {
+                        // Retrieve corresponding trait
+                        StPicoBTofPidTraits *trait = picoDst->btofPidTraits(picoTrack->bTofPidTraitsIndex());
+                        if (!trait)
+                            continue;
+
+                        // Fill 1/beta
+                        hist2[0][13]->Fill(picoTrack->pPtot() / picoTrack->charge(), 1. / trait->btofBeta());
+                        // fill squared mass
+                        hist2[0][16]->Fill(picoTrack->pPt() / picoTrack->charge(), (1 / pow(trait->btofBeta(), 2) - 1) * pow(picoTrack->pPt(), 2));
+                    }
+                }
+
+                // with cuts
+                if (!(picoTrack->isPrimary() && picoTrack->gDCA(event->primaryVertex()).Mag() < 3 &&
+                      event->primaryVertex().Z() < 25 && event->primaryVertex().Z() > -35 &&
+                      TMath::Sqrt(pow(event->primaryVertex().X(), 2) + pow(event->primaryVertex().Y(), 2)) < 2 &&
+                      picoTrack->pPt() < 5 &&
+                      picoTrack->pPt() > 0.015 &&
+                      TMath::Abs(picoTrack->pMom().Eta()) < 1 &&
+                      picoTrack->nHitsDedx() > 10 &&
+                      picoTrack->nHitsFit() > 15 &&
+                      flFit / flPoss > 0.52))
+
+                    continue;
+                multCounter++;
+                hist1[1][2]->Fill(picoTrack->pMom().Eta());
+                // fill primary track hits
+                hist1[1][3]->Fill(picoTrack->nHits());
+                hist1[1][4]->Fill(picoTrack->nHitsFit());
+                hist1[1][5]->Fill(picoTrack->nHitsMax());
+                hist1[1][6]->Fill(flFit / flPoss);
+                // fill DCA
+                hist1[1][7]->Fill(picoTrack->gDCA(event->primaryVertex()).Z());
+                hist1[1][8]->Fill(picoTrack->gDCA(event->primaryVertex()).Mag());
+                hist1[1][9]->Fill(picoTrack->gDCA(event->primaryVertex()).X(), picoTrack->gDCA(event->primaryVertex()).Y());
+                // fill dE/dx hits
+                hist1[1][10]->Fill(picoTrack->nHitsDedx());
+                // fill dEdx p/q
+                hist2[1][12]->Fill(picoTrack->pPtot() / picoTrack->charge(), picoTrack->dEdx());
+                // fill Phi vs Pt
+                hist2[1][14]->Fill(picoTrack->pPt(), picoTrack->pMom().Phi());
+                // fill Eta vs Pt
+                hist2[1][15]->Fill(picoTrack->pPt(), picoTrack->pMom().Eta());
+
+                // fill for TOF
+                if (picoTrack->isTofTrack())
+                {
+                    // Retrieve corresponding trait
+                    StPicoBTofPidTraits *trait = picoDst->btofPidTraits(picoTrack->bTofPidTraitsIndex());
+                    if (!trait)
+                        continue;
+                    // Fill 1/beta
+                    hist2[1][13]->Fill(picoTrack->pPtot() / picoTrack->charge(), 1. / trait->btofBeta());
+                    // fill squared mass
+                    hist2[1][16]->Fill(picoTrack->pPt() / picoTrack->charge(), (1 / pow(trait->btofBeta(), 2) - 1) * pow(picoTrack->pPt(), 2));
+                }
+                // fill pt spectra for each centality
+                double dEta = 1, dPt;
+                dPt = (hist_spectra[0]->GetBinCenter(hist_spectra[0]->GetNbinsX()) - hist_spectra[0]->GetBinCenter(0)) / hist_spectra[0]->GetNbinsX();
+                hist_spectra[cent9]->Fill(picoTrack->pMom().Pt(), 1. / (picoTrack->pMom().Pt()) * dPt * dEta);
+            }
+        }
+        // Filling multiplicity after cuts
+        if (TMath::Abs(event->primaryVertex().Z()) < 30 &&
+            TMath::Sqrt(pow(event->primaryVertex().X(), 2) + pow(event->primaryVertex().Y(), 2)) < 2)
+            hist1[1][0]->Fill(multCounter);
+        multCounter = 0;
     }
+    picoReader->Finish();
+    outputFile->Write();
+    outputFile->Close();
+    return 0;
 }
--- build_histos.h
+++ build_histos.h
@@ -11,35 +11,42 @@
 #include "TF1.h"
 #include "TMath.h"
 #include "TStyle.h"
-TH1F *hist1[2][12];
+TH1F *hist1[2][11];
 TH2F *hist2[2][6];
-TString config[2] = {"before","after "};
-TString name[18] = {"mult", "VtxZ", "Eta", "pHits", "pHitsFit", "pHitsMax",
-                    "pFitPoss", "gHits", "gHitsFit", "gHitsMax", "gFitPoss","pDedxHits",
+TH1F *hist_spectra[9];
+TString config[2] = {"before","after"};
+TString name[17] = {"mult", "VtxZ", "Eta", "pHits", "pHitsFit", "pHitsMax",
+                    "pFitPoss", "DCAz", "DCAmag", "DCAxy","pDedxHits",
                     "VtxX_VyxY", "pDedx", "reveraseBeta", "PhiPt", "pMomTrVEta", "sqaredMass"};
-Int_t bin[18] = {475, 100, 100, 80, 80, 80, 100, 80, 80, 80, 100, 40, 200, 1000, 1000, 1000, 200, 1000};
-Double_t xmin[18] = {0, -250, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0.1, -10, -2, -3, 0, 0, -3};
-Double_t xmax[18] = {475, 250, 3, 80, 80, 80, 1.1, 80, 80, 80, 1.1, 80, 10, 2, 3, 5, 10, 3};
-Double_t ymin[18] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -10, 0, 0.7, -3.5, -3, -0.5};
-Double_t ymax[18] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 50, 4, 3.5, 3, 1.5};
+Int_t bin[17] = {475, 100, 100, 80, 80, 80, 100, 80, 80, 100, 40, 200, 1000, 1000, 1000, 200, 1000};
+Double_t xmin[17] = {0, -250, -3, 0, 0, 0, 0, 0, 0, 0, 0.1, -10, -2, -3, 0, 0, -3};
+Double_t xmax[17] = {475, 250, 3, 80, 80, 80, 1.1, 80, 80, 1.1, 80, 10, 2, 3, 5, 10, 3};
+Double_t ymin[17] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -10, 0, 0.7, -3.5, -3, -0.5};
+Double_t ymax[17] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 50, 4, 3.5, 3, 1.5};
 
 
-int i,j;
+int i,j,k;
 
 void build()
 {   
     for (i = 0; i < 2; i++)
     {
-        for (j = 0; j < 18; j++)
+        for (j = 0; j < 17; j++)
         {
-            if (j < 12)
+            if (j < 11)
                 hist1[i][j] =new TH1F(Form("hist_%s_%s", name[j].Data(), config[i].Data()),
                     Form("%s_%s", name[j].Data(), config[i].Data()), bin[j], xmin[j], xmax[j]);
             else
                 hist2[i][j] = new TH2F(Form("hist_%s_%s", name[j].Data(), config[i].Data()),
                       Form("%s_%s", name[j].Data(), config[i].Data()), bin[j], xmin[j], xmax[j], bin[j], ymin[j], ymax[j]);
-        }       
+        }      
     }
+
+    for (k=0;k<9;k++)
+        {
+            hist_spectra[k] =new TH1F(Form("hist_spectra_cent%i",k),
+                    Form("Pt spectra for cent%i",k ), 1000, 0, 1000);
+        } 
 }
 
 
